0af2efd0aba80df5d0097db1563b68dd253d2625
EnricoCirilli <enrico.cirilli@gmail.com>
2023-11-13 22:04:20 +0100

    add: img

--- main.js
+++ main.js
@@ -6,16 +6,22 @@ const app = Vue.createApp({
         return{
             cart: 0,
             product: 'Forno Brisa',
-            image: 'assets/images/sourdough.png',
+            image: 'assets/images/bianco.jpeg',
             link : 'https://www.fornobrisa.it/it/2-forno-brisa',
             inventory: 8,
             ingredients : ['Ingredienti:','Farine Biologiche', 'Acqua Filtrata','Pasta Madre', 'Sale Integrale'],
-            
+            variants: [
+                {id: 2234, color:'bianco', image:'assets/images/bianco.jpeg'},
+                {id: 2235, color:'integrale', image: 'assets/images/integrale.jpeg'},
+            ]
         }
     },
     methods: {
         addToCart(){
             this.cart += 1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
         }
     }
 })
